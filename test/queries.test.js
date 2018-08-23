61f32f7f4985de82d9f51db0ef6170b820509bc8
zeanort <armandwork@hotmail.co.uk>
2018-08-23 12:25:46 +0100

    error handleing changes

--- test/queries.test.js
+++ test/queries.test.js
@@ -5,3 +5,17 @@ tape('tape is running', (t) => {
     t.equal(2 + 2, 4, '2 plus 2 is 4');
     t.end();
 });
+
+tape('Database is generating', (t) => {
+    runDbTestBuild()
+    .then( res => {
+        // console.log(res)
+        t.ok(res, 'Is true')
+        t.end()
+    
+    })
+    .catch(error => {
+        t.error(error, 'This should not be here, ERROR ERROR')
+        t.end()
+    })
+})
