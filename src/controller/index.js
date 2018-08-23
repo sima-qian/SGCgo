299051f21718afa3787e457b17ccedfda74a269e
Michael Watts <hello@michaelwatts.co>
2018-08-23 15:57:47 +0100

    fix catch block in error route

--- src/controller/index.js
+++ src/controller/index.js
@@ -1,15 +1,19 @@
 const express = require('express');
 
+// creates a new Express router
 const router = express.Router();
 
+// require controllers
 const landing = require('./landing');
 const getBoard = require('./get-board');
 const errorRoute = require('./error-route');
-
 const error = require('./error');
 
+// set home route
 router.get('/', landing.get);
+// show a specific board
 router.get('/board/:id', getBoard.get);
+
 router.get('/make_error', errorRoute);
 router.use(error.client);
 router.use(error.server);
--- src/database/db_connect.js
+++ src/database/db_connect.js
@@ -2,19 +2,19 @@ const { Pool } = require('pg');
 const url = require('url');
 const env = require('env2');
 env('./config.env'); // env starts in root so doesn't need two periods
-let DB_URL = process.env.DB_URL
+let DB_URL = process.env.DB_URL;
 
 if (process.env.NODE_ENV === 'test') {
-  console.log("running test environment");
-  DB_URL = process.env.TEST_DB_URL
+  console.log('running test environment');
+  DB_URL = process.env.TEST_DB_URL;
 }
 
-console.log('DB_URL: ', DB_URL)
+console.log('DB_URL: ', DB_URL);
 
-if (!DB_URL) throw new Error('Environment variable DB_URL must be set')
+if (!DB_URL) throw new Error('Environment variable DB_URL must be set');
 
-const params = url.parse(DB_URL)
-const [username, password] = params.auth.split(':')
+const params = url.parse(DB_URL);
+const [username, password] = params.auth.split(':');
 
 const options = {
   host: params.hostname,
@@ -23,8 +23,7 @@ const options = {
   max: process.env.DB_MAX_CONNECTIONS || 2,
   user: username,
   password,
-  ssl: params.hostname !== 'localhost'
-}
-console.log(options)
+  ssl: params.hostname !== 'localhost',
+};
 
-module.exports = new Pool(options)
+module.exports = new Pool(options);
